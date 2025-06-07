01029eab1f7b1615f419c8bebd5696437874b13d
reysilvaa <reynaldsilva123@gmail.com>
2025-06-08 02:05:39 +0700

    perbarui kolom paymentMethod pada tabel payment untuk menggunakan enum PaymentMethod baru dengan menghapus constraint enum lama dan mengatur ulang kolom

--- prisma/migrations/20250607182836_add_enums_paymentmethod/migration.sql
+++ prisma/migrations/20250607182836_add_enums_paymentmethod/migration.sql
@@ -1,1 +1,8 @@
 -- Migrasi kosong sementara 
+
+-- Modifikasi kolom paymentMethod pada tabel payment untuk menggunakan enum PaymentMethod baru
+-- Pertama, hapus constraint enum lama
+ALTER TABLE `payment` MODIFY `paymentMethod` VARCHAR(255);
+
+-- Kemudian, atur ulang kolom dengan enum baru
+ALTER TABLE `payment` MODIFY `paymentMethod` ENUM('gopay', 'shopeepay', 'qris', 'bca_va', 'bri_va', 'bni_va', 'permata_va', 'mandiri_bill', 'cimb_va', 'danamon_va', 'indomaret', 'alfamart', 'akulaku', 'kredivo', 'dana', 'credit_card', 'cash') NULL; 
