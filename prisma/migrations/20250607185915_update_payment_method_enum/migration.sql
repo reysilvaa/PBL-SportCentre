43e54db8ad5057894d23f56eb3cbb95c423a46b4
reysilvaa <reynaldsilva123@gmail.com>
2025-06-08 02:08:43 +0700

    perbaiki nama tabel menjadi 'Payment' pada migrasi kolom paymentMethod untuk menggunakan enum PaymentMethod baru

--- prisma/migrations/20250607185915_update_payment_method_enum/migration.sql
+++ prisma/migrations/20250607185915_update_payment_method_enum/migration.sql
@@ -5,4 +5,4 @@
 
 */
 -- AlterTable
-ALTER TABLE `payment` MODIFY `paymentMethod` ENUM('gopay', 'shopeepay', 'qris', 'bca_va', 'bri_va', 'bni_va', 'permata_va', 'mandiri_bill', 'cimb_va', 'danamon_va', 'indomaret', 'alfamart', 'akulaku', 'kredivo', 'dana', 'credit_card', 'cash') NULL;
+ALTER TABLE `Payment` MODIFY `paymentMethod` ENUM('gopay', 'shopeepay', 'qris', 'bca_va', 'bri_va', 'bni_va', 'permata_va', 'mandiri_bill', 'cimb_va', 'danamon_va', 'indomaret', 'alfamart', 'akulaku', 'kredivo', 'dana', 'credit_card', 'cash') NULL;
